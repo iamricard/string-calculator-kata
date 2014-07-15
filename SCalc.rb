0ec67d8c09cff60b595234a6039a3270a15ca923
Ricard Sole <ricard.solecasas@gmail.com>
2014-07-15 17:19:15 +0200

    basic refactor

--- SCalc.rb
+++ SCalc.rb
@@ -1,19 +1,15 @@
 class SCalc
-    def initialize
-    end
-
     def self.add(numbers)
       delimiter = find_delimiter(numbers)
       has_negatives(numbers, delimiter)
       if delimiter != ',' then numbers = numbers.split(delimiter, 2)[1] end
-      numbers = numbers.strip.gsub(/\n/, ',').split(delimiter)
-      result = numbers.reduce { |sum, n| sum.to_i + n.to_i }
-      result.to_i
+      numbers = numbers.strip.gsub(/\n/, delimiter).split(delimiter)
+      numbers.reduce { |sum, n| sum.to_i + n.to_i }.to_i
     end
 
     private
     def self.find_delimiter(numbers)
-      numbers.start_with? ? numbers[2] : ','
+      numbers.start_with?("//") ? numbers[2] : ','
     end
 
     def self.has_negatives(numbers, delimiter)
